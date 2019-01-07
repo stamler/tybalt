d6e0e28383c8cf24fac0859df0f6fe6b14bf43fd
Dean Stamler <dean@stamler.ca>
2019-01-07 14:21:30 -0500

    transition to firestore

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,33 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+const base = 'https://console.firebase.google.com/project/charade-ca63f/database/firestore/data'
 admin.initializeApp();
 
+const db = admin.firestore()
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
+// Realtime Database under the path /messages/:addId/original
 exports.addMessage = functions.https.onRequest((req, res) => {
     // Grab the text parameter.
     const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    // Add the message into the Cloud Firestore using the Firebase Admin SDK
+    return db.collection('/messages').add({original: original}).then((docRef) => {
       // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-      return res.redirect(303, snapshot.ref.toString());
+      return res.redirect(303, base + docRef.path);
     });
   });
   
-
-  // Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-.onCreate((snapshot, context) => {
+// Listens for new messages added to /messages/:addId/original and creates an
+// uppercase version of the message to /messages/:addId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{addId}')
+.onCreate((docSnap, context) => {
   // Grab the current value of what was written to the Realtime Database.
-  const original = snapshot.val();
-  console.log('Uppercasing', context.params.pushId, original);
+  const original = docSnap.data().original
+  console.log('Uppercasing', context.params.addId, original);
   const uppercase = original.toUpperCase();
   // You must return a Promise when performing asynchronous tasks inside a Functions such as
   // writing to the Firebase Realtime Database.
   // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-  return snapshot.ref.parent.child('uppercase').set(uppercase);
+  return docSnap.ref.update({uppercase: uppercase});
 });
