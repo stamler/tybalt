7134437784fa548051b0b3a8b9ca11c1e174d598
Dean Stamler <dean@stamler.ca>
2019-01-09 16:05:22 -0500

    Comments, basic validation and triage between RawLogins and Computers

--- functions/index.js
+++ functions/index.js
@@ -41,30 +41,69 @@ exports.makeUppercase = functions.firestore.document('/messages/{addId}')
 // incomplete write it to RawLogins collection for later processing
 exports.rawLogins = functions.https.onRequest((req, res) => {
 
-  // We expect the request to be sent with a content type application/json
+  // Reject requests with wrong Content-Type
   if (req.get('Content-Type') !== "application/json") {
     // per RFC 7231 https://tools.ietf.org/html/rfc7231#section-6.5.13
     return res.sendStatus(415)
   }
 
-  let d = req.body
+  // Add a timestamp sentinel to the body so it can be stored
   // See https://github.com/angular/angularfire2/issues/1292
+  let d = req.body
   d.datetime = admin.firestore.FieldValue.serverTimestamp()
 
-  // If the submission validates update Computers and Users
-  if (d.serial !== null && d.serial.length >= 4 && 
-      d.manufacturer !== null && d.manufacturer.length >= 2 &&
-      d.username !== null ) {
-    return db.collection('Computers').add(d)
-    .then((docRef) => { return res.sendStatus(202) })
-    .catch((error) => { return res.sendStatus(500) });
-  
+  // If the submission validates write or update Computers and Users
+  if (validateRawLogin(d)) {
+    // create a mfg-serial identifier slug
+    const docRef = db.collection('Computers').doc(generateSlug(d.serial, d.manufacturer))
+    return storeLoginEvent(docRef, d)
+      .then((docRef) => { return res.sendStatus(202) })
+      .catch((error) => { return res.sendStatus(500) });  
   } else {
-    // If the submission doesn't validate then 
-    // add to RawLogins for later processing
+    // Otherwise add to RawLogins for later processing
     return db.collection('RawLogins').add(d)
-    .then((docRef) => { return res.sendStatus(202) })
-    .catch((error) => { return res.sendStatus(500) });  
+      .then((docRef) => { return res.sendStatus(202) })
+      .catch((error) => { return res.sendStatus(500) });  
   }
 
 });
+
+function generateSlug(serial, mfg) {
+  return serial.trim() + ',' + mfg.toLowerCase().replace('.','').replace(',','')
+                          .replace('inc','').replace('ltd','')
+                          .trim().replace(' ','_');
+}
+
+function validateRawLogin(d) {
+  if (d.hasOwnProperty('serial') && d.hasOwnProperty('manufacturer') &&
+        d.hasOwnProperty('username') ) {
+    if (d.serial !== null && d.manufacturer !== null && d.username !== null ) {
+      if (d.serial.length>=4 && d.manufacturer.length>=2 && d.username.length>=4) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+function storeLoginEvent(docRef, data) {
+  //  set(): overwrite a doc or create it if it doesn't exist yet
+  //  set() with merge: update fields in doc or create it if it doesn't exists
+  //  update(): update fields, fail if doc doesn't exist
+  //  create(): create the document, fail if doc already exists
+
+  // Creates or updates Computers and Users collection (and maybe others)
+  // TODO: attempt an update and do an add() instead on error
+  // This is probably a situation for async/await
+  // https://stackoverflow.com/questions/50008619/firestore-then-function-not-executing-after-set
+
+  return docRef.get().then(function(doc) {
+    if (doc.exists) {
+      // Update and return void promise
+      return docRef.update(data);
+    } else {
+      // Create and return void promise
+      return docRef.set(data);
+    }
+  });
+}
