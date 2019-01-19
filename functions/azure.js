377819a094c8c5b18710fa1cd28b1449c139fc1e
Dean Stamler <dean@stamler.ca>
2019-01-18 19:51:09 -0500

    fold hasFreshCert() into getCertificates(), assert(), better comments

--- functions/azure.js
+++ functions/azure.js
@@ -21,6 +21,7 @@ const serverTimestamp = require('firebase-admin').firestore.FieldValue.serverTim
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 const jwkToPem = require('jwk-to-pem');
+const assert = require('assert');
 
 exports.handler = async (req, res, db) => {
 
@@ -39,7 +40,7 @@ exports.handler = async (req, res, db) => {
     console.log(`Valid token received for ${valid.name}`);
     return res.sendStatus(200)
   } else {
-    console.log("Invalid token");
+    console.log(`Invalid token: ${req.body.token}`);
     return res.status(401).send("Couldn't validate a token");
   }
 }
@@ -72,52 +73,49 @@ async function getCertificates(db) {
   const azureRef = db.collection('Cache').doc('azure');
   snap = await azureRef.get();
 
-  if (hasFreshCert(snap, 86400)) {
-    return snap.get('certificates');
-  } else {
-    // Load and cache fresh certificates from Microsoft
-
-    // Get the OpenID config from the Microsoft common URL
-    // Then use it to get Microsoft's up-to-date JWKs
-    let openIdConfigURI = 'https://login.microsoftonline.com/common/' +
-      '.well-known/openid-configuration';
-    let res;
-    try {
-      res = await axios.get(openIdConfigURI);
-      res = await axios.get(res.data.jwks_uri);
-    } catch (error) {
-      console.log("Error making external HTTP request. You on a free plan?");
-      throw error;
-    }
-    
-    // (re)build the certificates object with data returned from Microsoft
-    certificates = {};
-    for (let key of res.data.keys) {
-      certificates[key.kid] = jwkToPem(key);
-    }
-
-    // Update certificates in existing Cache/azure
-    // that we confirmed exists earlier
-    await azureRef.set({ retrieved: serverTimestamp(), 
-      certificates: certificates }, {merge: true} );
-    console.log("reloaded certificates");
-
-    return certificates;
-  }
-}
-
-// returns true if the firestore Document 
-// contains fresh Azure AD keys keys
-function hasFreshCert(snapshot, timeout) {
-  let retrieved = snapshot.get('retrieved');
-  let certificates = snapshot.get('certificates');
+  let retrieved = snap.get('retrieved');
+  let certificates = snap.get('certificates');
 
   if ( retrieved !== undefined && certificates !== undefined) {
-    // timeout arg is given in seconds, Date is in msec
-    if (Date.now() - retrieved.toDate() < timeout * 1000 ) {
-      console.log("Cached certificates less than " + timeout + " seconds old.");
-      return true;
+    // 1 day timeout in msec
+    if (Date.now() - retrieved.toDate() < 86400 * 1000 ) {
+      return certificates;
     }
+  } 
+  
+  // Load and cache fresh certificates from Microsoft
+
+  // Get the OpenID config from the Microsoft common URL
+  // Then use it to get Microsoft's up-to-date JWKs
+  let openIdConfigURI = 'https://login.microsoftonline.com/common/' +
+    '.well-known/openid-configuration';
+  let res;
+  try {
+    res = await axios.get(openIdConfigURI);
+    res = await axios.get(res.data.jwks_uri);
+  } catch (error) {
+    console.log("Error making external HTTP request. You on a free plan?");
+    throw error;
   }
-  return false;
+  
+  // build the certificates object with data from Microsoft
+  // at this point certificates should be undefined, confirm 
+  // and log if it isn't, probably a bug
+  try {
+    assert(certificates === undefined);    
+  } catch (error) {
+    console.log(error);  
+  }
+
+  certificates = {};
+  for (let key of res.data.keys) {
+    certificates[key.kid] = jwkToPem(key);
+  }
+
+  // Save certificates in the Cache/azure
+  await azureRef.set({ retrieved: serverTimestamp(), 
+    certificates: certificates }, {merge: true} );
+  console.log("reloaded certificates");
+
+  return certificates;  
 }
