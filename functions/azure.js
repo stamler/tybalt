e4923bafd6922bfbffc05508f018ef2d4cedb0c5
Dean Stamler <dean@stamler.ca>
2019-01-21 17:01:46 -0500

    documentation comments for synchronization of users.

--- functions/azure.js
+++ functions/azure.js
@@ -4,9 +4,10 @@ This module provides a handler() function that takes request and response
 objects and the admin firestore instance object as its arguments.
 
 The handler accepts only POST requests and examines them for an 
-id_token provided by Azure AD. If it finds a valid token, it creates a
-Firebase Custom Auth token that reflects the claims in the Azure token
-then returns this token to the client (somehow)
+id_token provided by Azure AD. If it finds a valid token, it creates or updates
+the user in Firebase Authentication then mints a Firebase Custom Auth token 
+that reflects the claims in the Azure token. Finally it returns 
+this token to the client
 
 About Azure ID tokens (from Microsoft):
 ID Tokens should be used to validate that a user is who they claim to be
@@ -36,8 +37,58 @@ exports.handler = async (req, res, db) => {
   catch (error) { return res.status(401).send(`${error}`); }
 
   if (valid !== null) {
+    // TODO: Get Azure Application ID from Firestore and verify
+    // that it matches valid.aud so we're not minting tokens for the wrong
+    // application. If there's no match, return HTTP 401 unauthorized with
+    // a message of "This token is valid but won't work for this Application"
+
+    // TODO: validate valid.nonce matches submitted value in the client app
+
+    // TODO: when minting the firebase token, include roles assigned in
+    // the firebase Users collection document for this user.
+    
+    /*
+
+    About The Firebase Auth collection:
+    Firebase Auth will use Azure Object ID as the key (uid, oid ) as this value
+    never changes. Upon receiving a valid Azure id_token with a previously 
+    unused oid, we will create a new user in Firebase auth with this value as
+    the key. We will also update properties related to the user, specifically
+    name and email. At this point the auth user will not be correlated with
+    the rest of the database.
+    
+    Issue: we need to have some way to durably correlate the auth user with 
+    properties in the Cloud Firestore database. However since the Azure 
+    Object ID isn't present in on-premesis logins and also since the ImmutableID
+    found in Azure (which does map to On-prem ms-DS-ConsistencyGuid) isn't 
+    exporable in the token, we cannot set up the correlation right away.
+
+    Instead, we correlate them in a Users collection with a key of 
+    ms-DS-ConsistencyGuid which contains a document for each User and can be 
+    populated by either computer Logins, Manually, or via Logins throught the
+    web app. The synchronized state is that a document contains at least an
+    Azure Object ID and an AD ms-DS-ConsistencyGuid. It can have other useful 
+    information for a profile including links to profile pictures.
+
+    /// Follow the soft match / hard match model
+      ==> https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-install-existing-tenant#sync-with-existing-users-in-azure-ad
+
+    When rawLogins is triggered if a document in Users already exists with a
+    key that matches the ms-DS-ConsistencyGuid that document is updated, 
+    otherwise it is created. If an auth user exists with the same upn/email,
+    that auth user's key will be written to the Users document and a soft-match
+    flag will be set.
+
+    */
+
     // TODO: mint a firebase custom token with the information from valid
-    // admin.createCustomToken()?
+    // 1. First get claims about the user from the valid token
+    const oid = valid.oid;
+    const claims = {name: valid.name, email: valid.email};
+
+    // admin.auth().updateUser()
+    // admin.auth().createUser()
+    // admin.auth().createCustomToken()
     console.log(`Valid token received for ${valid.name}`);
     return res.sendStatus(200)
   }
