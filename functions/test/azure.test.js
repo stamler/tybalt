44956af7e8800ff69ac805623a34f6c0837bb9e3
Dean Stamler <dean@stamler.ca>
2019-01-28 17:02:36 -0500

    comments

--- functions/test/azure.test.js
+++ functions/test/azure.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+// Generate keys here https://mkjwk.org
+
 describe("azure module", () => {
   const azureModule = require('../azure.js');
   it("verifies that a user is authorized", () => {
