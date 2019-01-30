15269b9d38b5ae891779d717a89100e21c77decb
Dean Stamler <dean@stamler.ca>
2019-01-30 10:01:49 -0500

    remove boilerplate

--- functions/test/azure.test.js
+++ functions/test/azure.test.js
@@ -33,36 +33,30 @@ describe("azure module", () => {
   
   describe("handler()", () => {
     const handler = azureModule.handler;
-    
+    const makeResponseObject = () => { return { header: sinon.spy(), status: sinon.stub().returnsThis(), send: sinon.stub().returnsThis() }; }; 
     let clock; // declare sinon's clock and try to restore after each test
     afterEach(() => { try { clock.restore(); } catch (e) {} }); 
 
-    // TODO: rewrite azure.js to always use status().send() instead of sendStatus() for simplified testing
     it("responds (405 Method Not Allowed) if request method isn't POST", async () => {
-      const req = {};
-      const res = { header: sinon.spy(), status: sinon.stub().returnsThis(), send: sinon.stub().returnsThis() };
-      let result = await handler(req, res);      
+      let result = await handler({}, makeResponseObject());      
       assert.deepEqual(result.header.args[0], ['Allow','POST']);
       assert.equal(result.status.args[0],405);
     });
     it("responds (415 Unsupported Media Type) if Content-Type is not application/json", async () => {
       const req = { method:'POST', get: sinon.stub().withArgs('Content-Type').returns('not/json') };
-      const res = { header: sinon.spy(), status: sinon.stub().returnsThis(), send: sinon.stub().returnsThis() };
-      let result = await handler(req, res);
+      let result = await handler(req, makeResponseObject());
       assert.equal(result.status.args[0], 415);
     });
     it("responds (401 Unauthorized) if id_token property is missing from request", async () => {
       const req = { method:'POST', body: {}, get: sinon.stub().withArgs('Content-Type').returns('application/json') };
-      const res = { status: sinon.stub().returnsThis(), send: sinon.stub().returnsThis() };
-      let result = await handler(req, res);
+      let result = await handler(req, makeResponseObject());
       assert.equal(result.status.args[0],401);
       assert.equal(result.send.args[0],"no id_token provided");
     });
     it("responds (401 Unauthorized) if id_token in request is unparseable", async () => {
       const req = { method:'POST', body: { id_token: "fhqwhgads" }, get: sinon.stub().withArgs('Content-Type').returns('application/json') };
-      const res = { status: sinon.stub().returnsThis(), send: sinon.stub().returnsThis() };
       // TODO: stub out getCertificates() for testing so db can be empty object
-      let result = await handler(req, res)
+      let result = await handler(req, makeResponseObject());
     });
     it("responds (401 Unauthorized) if id_token in request is expired", () => {
       clock = sinon.useFakeTimers(1546305800000); // Jan 1, 2019 01:23:20 UTC
