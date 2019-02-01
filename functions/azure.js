3bd1268c3dfba1d28b298d9d8e6f909bcdf081b7
Dean Stamler <dean@stamler.ca>
2019-02-01 10:31:28 -0500

    remove extraneous valid !== null check branch and final return 500

--- functions/azure.js
+++ functions/azure.js
@@ -42,91 +42,87 @@ exports.handler = async (req, res, options={}) => {
   }
 
   // validate azure token from request body
-  let valid = null;
+  let valid;
   try { valid = await validAzureToken(req.body.id_token, certificates); }
   catch (error) { return res.status(401).send(`${error}`); }
 
-  if (valid !== null) {
-
-    // Check the app_id
-    if ( app_id && valid.aud !== app_id ) {
-      return res.status(403).send("AudienceError: Provided token invalid for this application");
-    }
-  
-    // Check the tenant_ids array
-    if (tenant_ids.length > 0) {
-      const iss_guid = valid.iss.match(/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i);
-      if (iss_guid && !tenant_ids.includes(iss_guid[0])) {
-        // GUID extracted from the iss claim IS NOT in the array of tenant_ids
-        return res.status(403).send("IssuerError: Provided token issued by foreign tenant");
-      }
-    }
-    
-    /*
-    
-    Issue: we need to have some way to durably correlate the auth user with 
-    properties in the Cloud Firestore database. However since the Azure 
-    Object ID isn't present in on-premesis logins and also since the ImmutableID
-    found in Azure (which does map to On-prem sourceAnchor) isn't 
-    a claim in the token, we cannot set up the correlation right away.
-
-    Instead, we correlate them in a Users collection with a key of 
-    sourceAnchor which contains a document for each User and can be 
-    populated by either computer Logins, Manually, or via Logins throught the
-    web app. The synchronized state is that a document contains at least an
-    Azure Object ID and an AD sourceAnchor. It can have other useful 
-    information for a profile including links to profile pictures.
-
-    A Users document can only be created by the rawLogins function or manually
-    When it's being created it checks for a auth() object with matching email
-    and if that object exists assigns its key to its own azure_oid property
-
-    An auth() user can only be created by the auth().createUser() method. 
-    Using functions.auth.user().onCreate() event handler to updating the
-    corresponding Users document with matching email is one approach.
-
-    Similarly functions.auth.user().onDelete() could be used to strip the 
-    Azure Object ID from the corresponding Users document were a deletion to
-    occur
-
-    /// Follow the soft match / hard match model
-      ==> https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-install-existing-tenant#sync-with-existing-users-in-azure-ad
-
-    When rawLogins is triggered if a document in Users already exists with a
-    key that matches the sourceAnchor that document is updated, 
-    otherwise it is created. If an auth user exists with the same upn/email,
-    that auth user's key will be written to the Users document and a soft-match
-    flag will be set.
-
-    */
-
-    // Create or update the user then mint a firebase custom token for the user
-    // TODO: consider breaking this into a function for testability. Specific 
-    // issues include depending on firebase admin functions
-    const uid = valid.oid;
-    const properties = {displayName: valid.name, email: valid.email};
-
-    let userRecord;
-    try {
-      // try updating the user first
-      userRecord = await admin.auth().updateUser(uid, properties);      
-    } catch (error) {
-      if (error.code === 'auth/user-not-found') {
-        // if the user doesn't exist then create it
-        userRecord = await admin.auth().createUser({ uid, ...properties});
-      } else if (error.code === 'auth/email-already-exists') {
-        return res.status(501).send("A user with this email address and a different Object ID already exists. Either the user who used to use that email address must sign in to update their address and and fix the conflict, or an administrator must delete that user's auth account in the database so the new user can assume the email address.");
-      }
-    }
-    
-    // TODO: when minting the firebase token, include roles assigned in
-    // the firebase Users collection document for this user.
-    // mint the token
-    const firebaseCustomToken = await admin.auth().createCustomToken(userRecord.uid);
-    return res.status(200).send(firebaseCustomToken);
+  // Check the app_id
+  // This functionality can be rolled into options on jwt.verify()
+  if ( app_id && valid.aud !== app_id ) {
+    return res.status(403).send("AudienceError: Provided token invalid for this application");
   }
 
-  return res.status(500).send();
+  // Check the tenant_ids array
+  if (tenant_ids.length > 0) {
+    const iss_guid = valid.iss.match(/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i);
+    if (iss_guid && !tenant_ids.includes(iss_guid[0])) {
+      // GUID extracted from the iss claim IS NOT in the array of tenant_ids
+      return res.status(403).send("IssuerError: Provided token issued by foreign tenant");
+    }
+  }
+  
+  /*
+  
+  Issue: we need to have some way to durably correlate the auth user with 
+  properties in the Cloud Firestore database. However since the Azure 
+  Object ID isn't present in on-premesis logins and also since the ImmutableID
+  found in Azure (which does map to On-prem sourceAnchor) isn't 
+  a claim in the token, we cannot set up the correlation right away.
+
+  Instead, we correlate them in a Users collection with a key of 
+  sourceAnchor which contains a document for each User and can be 
+  populated by either computer Logins, Manually, or via Logins throught the
+  web app. The synchronized state is that a document contains at least an
+  Azure Object ID and an AD sourceAnchor. It can have other useful 
+  information for a profile including links to profile pictures.
+
+  A Users document can only be created by the rawLogins function or manually
+  When it's being created it checks for a auth() object with matching email
+  and if that object exists assigns its key to its own azure_oid property
+
+  An auth() user can only be created by the auth().createUser() method. 
+  Using functions.auth.user().onCreate() event handler to updating the
+  corresponding Users document with matching email is one approach.
+
+  Similarly functions.auth.user().onDelete() could be used to strip the 
+  Azure Object ID from the corresponding Users document were a deletion to
+  occur
+
+  /// Follow the soft match / hard match model
+    ==> https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-install-existing-tenant#sync-with-existing-users-in-azure-ad
+
+  When rawLogins is triggered if a document in Users already exists with a
+  key that matches the sourceAnchor that document is updated, 
+  otherwise it is created. If an auth user exists with the same upn/email,
+  that auth user's key will be written to the Users document and a soft-match
+  flag will be set.
+
+  */
+
+  // Create or update the user then mint a firebase custom token for the user
+  // TODO: consider breaking this into a function for testability. Specific 
+  // issues include depending on firebase admin functions
+  const uid = valid.oid;
+  const properties = {displayName: valid.name, email: valid.email};
+
+  let userRecord;
+  try {
+    // try updating the user first
+    userRecord = await admin.auth().updateUser(uid, properties);      
+  } catch (error) {
+    if (error.code === 'auth/user-not-found') {
+      // if the user doesn't exist then create it
+      userRecord = await admin.auth().createUser({ uid, ...properties});
+    } else if (error.code === 'auth/email-already-exists') {
+      return res.status(501).send("A user with this email address and a different Object ID already exists. Either the user who used to use that email address must sign in to update their address and and fix the conflict, or an administrator must delete that user's auth account in the database so the new user can assume the email address.");
+    }
+  }
+  
+  // TODO: when minting the firebase token, include roles assigned in
+  // the firebase Users collection document for this user.
+  // mint the token
+  const firebaseCustomToken = await admin.auth().createCustomToken(userRecord.uid);
+  return res.status(200).send(firebaseCustomToken);
 }
 
 // returns the decoded payload of valid token. Caller must handle exceptions
