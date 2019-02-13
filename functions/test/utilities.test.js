a01b7bbadc81fd931d5d80af3ec7f916221f9271
Dean Stamler <dean@stamler.ca>
2019-02-13 16:09:05 -0500

    delete code and tests for filterProperties() utility function

--- functions/test/utilities.test.js
+++ functions/test/utilities.test.js
@@ -2,59 +2,6 @@ const assert = require('chai').assert;
 
 describe("utilities module", () => {
   const utilitiesModule = require('../utilities.js')
-  describe("filterProperties()", () => {
-    // Test setup for filterProperties() in utilities.js
-    const data = { taste:null, fruit: "apple", vegetable: "carrot", mineral: null };
-    const validProps = ["fruit", "vegetable", "mineral", "missing"];
-    const requiredProps = ["vegetable"];
-    const filterProperties = utilitiesModule.filterProperties;
-    it("returns empty object if no options are provided", () => {
-      const expected = {};
-      return assert.deepEqual(filterProperties(data), expected);
-    });
-    it("keeps all valid properties with default args", () => {
-      const options = { valid: validProps }
-      const expected = {fruit: "apple", vegetable: "carrot", mineral: null};
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-    it("strips valid null properties if stripValidNulls is true", () => {
-      const options = {valid: validProps, stripValidNulls: true};
-      const expected = {fruit: "apple", vegetable: "carrot"};
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-    it("keeps required properties though valid properties aren't given", () => {
-      const options = {required: requiredProps};
-      const expected = {vegetable: "carrot"};
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-    it("throws error if required value(s) are null while allowAndAddRequiredNulls is false", () => {
-      const options = {required:["taste", "fruit"], allowAndAddRequiredNulls: false };
-      // first arg of throws() must be function, so wrap it with params
-      const wrapped = () => { filterProperties(data, options) };
-      return assert.throws(wrapped, Error);
-    });
-    it("throws error if required value(s) are missing while allowAndAddRequiredNulls is false", () => {
-      const options = {required:["fruit", "extra"], allowAndAddRequiredNulls: false };
-      // first arg of throws() must be function, so wrap it with params
-      const wrapped = () => { filterProperties(data, options) };
-      return assert.throws(wrapped, Error);
-    });
-    it("keeps required null properties by default", () => {
-      const options = {required:["taste", "fruit"]};
-      const expected = {taste: null, fruit:"apple"};
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-    it("adds missing required properties as null if allowAndAddRequiredNulls is true", () => {
-      const options = {required:["taste", "fruit", "extra"], allowAndAddRequiredNulls:true};
-      const expected = {taste: null, fruit:"apple", "extra":null};
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-    it("strips valid null properties while adding required null properties", () => {
-      const options = {valid: validProps, required: ["vegetable", "taste", "extra"], stripValidNulls: true, allowAndAddRequiredNulls:true};
-      const expected = {fruit:"apple", vegetable:"carrot", taste:null, extra:null}
-      return assert.deepEqual(filterProperties(data, options), expected);
-    });
-  });
   describe("makeSlug()", () => {
     // Test setup for makeSlug() in utilities.js
     const makeSlug = utilitiesModule.makeSlug;
