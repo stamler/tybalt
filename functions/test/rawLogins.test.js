b5b45972ddbc4e4727a8140a3561e44e2972ca88
Dean Stamler <dean@stamler.ca>
2019-02-13 16:03:05 -0500

    define handler once rather than on every test

--- functions/test/rawLogins.test.js
+++ functions/test/rawLogins.test.js
@@ -19,20 +19,18 @@ describe("rawLogins module", () => {
     const sts = shared.stripTimestamps; // utility function to strip timestamp props
     const userObjArg = { givenName: undefined, surname: undefined, upn: "ttesterson@testco.co" , email: "ttesterson@testco.co", lastComputer:"SN123,manufac", userSourceAnchor:"f25d2a25f25d2a25f25d2a25f25d2a25" };
     const loginObjArg = {computer: "SN123,manufac", givenName: undefined, surname: undefined, userSourceAnchor: "f25d2a25f25d2a25f25d2a25f25d2a25" };
+    const handler = require('../rawLogins.js').handler;
 
     it("(405 Method Not Allowed) if request method isn't POST", async () => {
-      handler = require('../rawLogins.js').handler;
       let result = await handler(Req({method:'GET'}), Res(), makeDb());      
       assert.deepEqual(result.header.args[0], ['Allow','POST']);
       assert.equal(result.status.args[0][0],405);
     });
     it("(415 Unsupported Media Type) if Content-Type isn't application/json", async () => {
-      handler = require('../rawLogins.js').handler;
       let result = await handler(Req({contentType:'not/json'}), Res(), makeDb());
       assert.equal(result.status.args[0][0], 415);
     });
     it("(202 Accepted) if a valid JSON login is POSTed, neither computer nor user exists", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb();
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 202);
@@ -42,7 +40,6 @@ describe("rawLogins module", () => {
       sinon.assert.calledOnce(db.batchStubs.commit);
     });
     it("(202 Accepted) if a valid JSON login is POSTed, user exists", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb({userMatches: 1});
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 202);
@@ -52,7 +49,6 @@ describe("rawLogins module", () => {
       sinon.assert.calledOnce(db.batchStubs.commit);
     });
     it("(202 Accepted) if a valid JSON login is POSTed, computer exists", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb({computerExists: true});
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 202);
@@ -62,7 +58,6 @@ describe("rawLogins module", () => {
       sinon.assert.calledOnce(db.batchStubs.commit);
     });
     it("(202 Accepted) if a valid JSON login is POSTed, both computer and user exist", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb({computerExists: true, userMatches: 1});
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 202);
@@ -72,7 +67,6 @@ describe("rawLogins module", () => {
       sinon.assert.calledOnce(db.batchStubs.commit);
     });
     it("(202 Accepted) if a valid JSON login is POSTed, computer exists, multiple users match", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb({computerExists: true, userMatches: 2});
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 202);
@@ -82,7 +76,6 @@ describe("rawLogins module", () => {
       // assert batch.commit() was called once
     });
     it("(202 Accepted) if an invalid JSON login is POSTed", async () => {
-      handler = require('../rawLogins.js').handler;
       let result = await handler(Req({body: {}}),Res(), makeDb());
       assert.equal(result.status.args[0][0], 202);
       // assert set() was called once with args {} outside of batch
@@ -91,7 +84,6 @@ describe("rawLogins module", () => {
       // assert batch.commit() wasn't called
     });
     it("(500 Internal Server Error) if database write fails", async () => {
-      handler = require('../rawLogins.js').handler;
       const db = makeDb({writeFail: true});
       let result = await handler(Req({body: {...data}}),Res(), db);
       assert.equal(result.status.args[0][0], 500);
