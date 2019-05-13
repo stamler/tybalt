d66d010bcff0e5e5cee47477fa801469be065274
Dean Stamler <dean@stamler.ca>
2019-05-13 14:03:08 -0400

    basic testing of computers.js (no written tests)

---
+++ functions/test/computers.test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const shared = require('./shared.helpers.test');
+
+describe("computers module", () => {
+
+  const admin = require('firebase-admin');
+  const makeAuthStub = shared.makeAuthStub; // Stub admin.auth()
+  const makeDb = shared.makeFirestoreStub; // Stub admin.firestore()
+  const sandbox = sinon.createSandbox();
+
+  let db;
+  // eslint-disable-next-line prefer-arrow-callback
+  beforeEach(function() {
+    db = makeDb();
+    sandbox.stub(admin, 'auth').get( makeAuthStub() );
+  });
+
+  // eslint-disable-next-line prefer-arrow-callback
+  afterEach(function() { 
+    sandbox.restore();
+  });
+
+  describe("assignComputerToUser()", () => {
+    const assignComputerToUser = require('../computers.js').assignComputerToUser;
+    it("rejects if the user isn't authenticated", async () => {
+      const result = assignComputerToUser(undefined, {}, db);
+
+    });
+  });
+});
