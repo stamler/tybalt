d802869a564c79698bcdfe92e72ee26d5740fdb3
Dean Stamler <dean@stamler.ca>
2019-05-23 16:49:27 -0400

    create Projects component, TimeEntries rules

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    <h1>Projects</h1>
+  <div id="container">
+    <div>
+      <input type="textbox" placeholder="search..." v-model="search" />
+      <button>New Project</button>
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>{{ processedItems.length }}</th>
+          <th><a href="#" v-on:click="sort('job')">Job</a></th>
+          <th><a href="#" v-on:click="sort('manager')">Project Manager</a></th>
+          <th><a href="#" v-on:click="sort('client')">Client</a></th>
+          <th><a href="#" v-on:click="sort('proposal')">Proposal</a></th>
+          <th>Description</th>
+          <th><a href="#" v-on:click="sort('status')">Status</a></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in processedItems" v-bind:key="item.id">
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import firebase from "@/firebase";
+const db = firebase.firestore();
+const items = db.collection("Projects");
+import componentMaker from "../components/shared.js";
+
+const component = componentMaker(items);
+
+export default component;
+</script>
+
+<style scoped>
+#container {
+  text-align: left;
+}
+</style>
