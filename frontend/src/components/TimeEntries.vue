4c64751858cddeb4ae772ec0b80d0aae1f7190a0
Dean Stamler <dean@stamler.ca>
2019-05-30 12:43:31 -0400

    reorganize TimeEntries.vue

--- frontend/src/components/TimeEntries.vue
+++ frontend/src/components/TimeEntries.vue
@@ -11,11 +11,10 @@
           <th>{{ processedItems.length }}</th>
           <th><a href="#" v-on:click="sort('date')">Date</a></th>
           <th><a href="#" v-on:click="sort('job')">Job/Proposal</a></th>
-          <th><a href="#" v-on:click="sort('code')">Code</a></th>
-          <th><a href="#" v-on:click="sort('jobHours')">Job Hours</a></th>
+          <th><a href="#" v-on:click="sort('division')">Division</a></th>
+          <th><a href="#" v-on:click="sort('timetype')">Type</a></th>
           <th><a href="#" v-on:click="sort('hours')">Hours</a></th>
-          <th><a href="#" v-on:click="sort('mealsHours')">Meals Hours</a></th>
-          <th><a href="#" v-on:click="sort('workRecord')">Work Record</a></th>
+          <th><a href="#" v-on:click="sort('workrecord')">Work Record</a></th>
           <th><a href="#" v-on:click="sort('project')">Project</a></th>
           <th><a href="#" v-on:click="sort('description')">Description</a></th>
           <th><a href="#" v-on:click="sort('comments')">Comments</a></th>
@@ -26,11 +25,10 @@
           <td></td>
           <td>{{ item.date.toDate() | shortDate }}</td>
           <td>{{ item.job }}</td>
-          <td>{{ item.code }}</td>
-          <td>{{ item.jobHours }}</td>
+          <td>{{ item.division }}</td>
+          <td>{{ item.timetype }}</td>
           <td>{{ item.hours }}</td>
-          <td>{{ item.mealsHours }}</td>
-          <td>{{ item.workRecord }}</td>
+          <td>{{ item.workrecord }}</td>
           <td>{{ item.project }}</td>
           <td>{{ item.description }}</td>
           <td>{{ item.comments }}</td>
