dce523e2786030a14ec2e02b8616bfc11ba1c923
Dean Stamler <dean@stamler.ca>
2019-06-03 12:34:59 -0400

    remove shared.js dependency for List.vue

--- frontend/src/components/List.vue
+++ frontend/src/components/List.vue
@@ -55,16 +55,103 @@
 </template>
 
 <script>
-import componentMaker from "../components/shared.js";
+import moment from "moment";
+import { mapState } from "vuex";
+import Editor from "./Editor";
+import firebase from "@/firebase";
+const db = firebase.firestore();
 
-const component = componentMaker();
-
-component.created = function() {
-  this.schema = this.$parent.schema;
-  this.collection = this.$parent.collection;
-  this.items = this.$parent.items;
-  this.$bind("items", this.items);
+export default {
+  components: {
+    Editor
+  },
+  data() {
+    return {
+      schema: null, // schema: a reference to the parent schema
+      collection: null, // collection: a reference to the parent collection
+      showNewItem: false, // show or hide the Editor for new item
+      editingObject: {}, // An object of the firestore collection
+      taskAreaMode: "default",
+      items: [],
+      search: "",
+      sortBy: "created",
+      sortDescending: true,
+      selectAll: false,
+      selected: []
+    };
+  },
+  created() {
+    this.schema = this.$parent.schema;
+    this.collection = this.$parent.collection;
+    this.items = this.$parent.items;
+    this.$bind("items", this.items);
+  },
+  filters: {
+    shortDate(date) {
+      return moment(date).format("MMM DD");
+    },
+    dateFormat(date) {
+      return moment(date).format("YYYY MMM DD / HH:mm:ss");
+    },
+    relativeTime(date) {
+      return moment(date).fromNow();
+    }
+  },
+  computed: {
+    processedItems() {
+      return this.items
+        .slice() // shallow copy https://github.com/vuejs/vuefire/issues/244
+        .sort((a, b) => {
+          const direction = this.sortDescending ? -1 : 1;
+          if (a[this.sortBy] < b[this.sortBy]) return -1 * direction;
+          else if (a[this.sortBy] > b[this.sortBy]) return 1 * direction;
+          return 0;
+        })
+        .filter(
+          p => this.searchString(p).indexOf(this.search.toLowerCase()) >= 0
+        );
+    },
+    // the spread operator allows including the mapped computed properties
+    ...mapState(["claims", "state", "user"])
+  },
+  methods: {
+    toggleAll() {
+      this.selected = [];
+      if (!this.selectAll) {
+        for (let i in this.items) {
+          this.selected.push(this.items[i].id);
+        }
+      }
+    },
+    sort(property) {
+      this.sortDescending = !this.sortDescending;
+      this.sortBy = property;
+    },
+    searchString(item) {
+      return Object.values(item)
+        .join(",")
+        .toLowerCase();
+    },
+    clearEditor() {
+      this.editingObject = {};
+    },
+    deleteSelected() {
+      const batch = db.batch();
+      this.selected.forEach(key => {
+        // apparently Vuefire $bind on items removes the doc() function so we
+        // use collection instead of items. TODO: research and understand this
+        batch.delete(this.collection.doc(key));
+      });
+      batch
+        .commit()
+        .then(() => {
+          this.selected = [];
+          this.selectAll = false;
+        })
+        .catch(err => {
+          console.log(`Batch failed: ${err}`);
+        });
+    }
+  }  
 }
-
-export default component;
 </script>
