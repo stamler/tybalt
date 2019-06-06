c051001dc640f2e5677049aa430ab5deaf74cc19
Dean Stamler <dean@stamler.ca>
2019-06-06 15:05:56 -0400

    Update TimeEntries.vue to use List component

--- frontend/src/components/TimeEntries.vue
+++ frontend/src/components/TimeEntries.vue
@@ -1,53 +1,48 @@
 <template>
-  <div id="container">
-    <div>
-      <input type="textbox" placeholder="search..." v-model="search" />
+  <List>
+    <template v-slot:taskAreaDefault>
       <button>New Entry</button>
       <button>Check Entries</button>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th>{{ processedItems.length }}</th>
-          <th><a href="#" v-on:click="sort('date')">Date</a></th>
-          <th><a href="#" v-on:click="sort('job')">Job/Proposal</a></th>
-          <th><a href="#" v-on:click="sort('division')">Division</a></th>
-          <th><a href="#" v-on:click="sort('timetype')">Type</a></th>
-          <th><a href="#" v-on:click="sort('hours')">Hours</a></th>
-          <th><a href="#" v-on:click="sort('workrecord')">Work Record</a></th>
-          <th><a href="#" v-on:click="sort('project')">Project</a></th>
-          <th><a href="#" v-on:click="sort('description')">Description</a></th>
-          <th><a href="#" v-on:click="sort('comments')">Comments</a></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in processedItems" v-bind:key="item.id">
-          <td></td>
-          <td>{{ item.date.toDate() | shortDate }}</td>
-          <td>{{ item.job }}</td>
-          <td>{{ item.division }}</td>
-          <td>{{ item.timetype }}</td>
-          <td>{{ item.hours }}</td>
-          <td>{{ item.workrecord }}</td>
-          <td>{{ item.project }}</td>
-          <td>{{ item.description }}</td>
-          <td>{{ item.comments }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    </template>
+  </List>
 </template>
 
 <script>
 import firebase from "@/firebase";
-import store from "../store";
 const db = firebase.firestore();
-import componentMaker from "./shared.js";
+import store from "../store";
+import { mapState } from "vuex";
+import moment from "moment";
+import List from "./List";
 
-const component = componentMaker();
-component.created = function() {
-  this.$bind("items", db.collection("TimeEntries").where("uid", "==", store.state.user.uid));
+export default {
+  components: { List },
+  data() {
+    return {
+      schema: {
+        date: {
+          display: "Date",
+          derivation: obj => moment(obj.date.toDate()).format("MMM DD")
+        },
+        job: {display: "Job/Proposal"},
+        division: {display: "Division"},
+        timetype: {display: "Type"},
+        hours: {display: "Hours"},
+        workrecord: {display: "Work Record"},
+        project: {display: "Project"},
+        description: {display: "Description"},
+        comments: {display: "Comments"},
+      },
+      collection: db.collection("TimeEntries"),
+      items: db.collection("TimeEntries").where("uid", "==", store.state.user.uid),
+    }
+  },
+  computed: mapState(["claims"]),
+  created() {
+    // Modify UI based on permissions and business requirements here
+    this.create = this.select = this.del = this.edit =
+      this.claims.hasOwnProperty("users") &&
+      this.claims["users"] === true
+  }
 }
-
-export default component;
 </script>
