ca88cdeecf894d97720566b5f9a28f639037748c
Dean Stamler <dean@stamler.ca>
2019-06-06 14:30:19 -0400

    Simplify routing for Logins

--- frontend/src/components/Logins.vue
+++ frontend/src/components/Logins.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="list">List</router-link>&nbsp;
-      <router-link v-if="create" to="add">New</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <List/>
 </template>
 
 <script>
@@ -13,14 +7,12 @@ import firebase from "@/firebase";
 const db = firebase.firestore();
 import { mapState } from "vuex";
 import moment from "moment";
+import List from "./List";
 
 export default {
+  components: { List },
   data() {
     return {
-      create: false,
-      select: false,
-      edit: false,
-      del: false,
       schema: {
         givenName: {display: "First Name"},
         surname: {display: "Last Name"},
