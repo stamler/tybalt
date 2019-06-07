e857c5a4e05908fd848c520894d5dcbab7ff4408
Dean Stamler <dean@stamler.ca>
2019-06-07 14:34:37 -0400

    FIX: add claims to mapState

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -24,7 +24,7 @@ export default {
       links: this.$router.options.routes.filter(x => x.name)
     };
   },
-  computed: mapState(["user"]),
+  computed: mapState(["user","claims"]),
   methods: {
     signOut,
     showLink (link) {
