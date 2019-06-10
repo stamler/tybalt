df950257ac0ee6933784c1e0c862eac04204bdd9
Dean Stamler <dean@stamler.ca>
2019-06-10 18:02:39 -0400

    replace Computers with Computers2, del shared.js

--- frontend/src/components/Computers.vue
+++ frontend/src/components/Computers.vue
@@ -1,86 +1,95 @@
 <template>
-  <div id="container">
-    <div>
-      <input type="textbox" placeholder="search..." v-model="search" />
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th>{{ processedItems.length }}</th>
-          <th><a href="#" v-on:click="sort('computerName')">Computer</a></th>
-          <th><a href="#" v-on:click="sort('osVersion')">Windows</a></th>
-          <th><a href="#" v-on:click="sort('mfg')">mfg</a></th>
-          <th><a href="#" v-on:click="sort('model')">model</a></th>
-          <th><a href="#" v-on:click="sort('userGivenName')">First</a>&nbsp;<a href="#" v-on:click="sort('userSurname')">Last</a></th>
-          <th>assigned</th>
-          <th><a href="#" v-on:click="sort('updated')">updated</a></th>
-          <th><a href="#" v-on:click="sort('created')">created</a></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in processedItems" v-bind:key="item.id">
-          <td></td>
-          <td>
-            <span v-if="!item.computerName.includes(item.serial)">❗({{ item.serial }})</span>
-            <br/>
-            {{ item.computerName }}
-          </td>
-          <td>{{ item.osVersion }}</td>
-          <td>{{ item.mfg }}</td>
-          <td>{{ item.model }}</td>
-          <td>{{ item.userGivenName }} {{ item.userSurname }}</td>
-          <td>
-            <span v-if="!item.assigned">
-              <!-- Show this if the device has no assignment -->
-              <button 
-                v-if="claims.computers === true"
-                v-on:click="assign(item.id, item.userSourceAnchor)">
-                assign
-              </button>
-            </span>
-            <span v-else-if="item.assigned.userSourceAnchor !== item.userSourceAnchor">
-              <!-- Show this if the device has an assignment that doesn't
-              match the last user login -->
-              <button 
-                v-on:click="assign(item.id, item.userSourceAnchor)">
-                ❗assign, currently {{ item.assigned.givenName }} {{ item.assigned.surname }}
-              </button>
-            </span>
-            <span v-else>{{ item.assigned.time.toDate() | relativeTime }}</span>
-          </td>
-          <td>{{ item.updated.toDate() | relativeTime }}</td>
-          <td>{{ item.created.toDate() | dateFormat }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <List>
+    <template v-slot:columns="{ item }">
+      <td>
+        <span v-if="!item.computerName.includes(item.serial)">❗({{ item.serial }})</span>
+        <br/>
+        {{ item.computerName }}
+      </td>
+      <td>{{ item.osVersion }}</td>
+      <td>{{ item.mfg }}</td>
+      <td>{{ item.model }}</td>
+      <td>{{ item.userGivenName }}</td>
+      <td>{{ item.userSurname }}</td>
+      <td>
+        <span v-if="!item.assigned">
+          <!-- Show this if the device has no assignment -->
+          <button 
+            v-if="claims.computers === true"
+            v-on:click="assign(item.id, item.userSourceAnchor)">
+            assign
+          </button>
+        </span>
+        <span v-else-if="item.assigned.userSourceAnchor !== item.userSourceAnchor">
+          <!-- Show this if the device has an assignment that doesn't
+          match the last user login -->
+          <button 
+            v-on:click="assign(item.id, item.userSourceAnchor)">
+            ❗assign, currently {{ item.assigned.givenName }} {{ item.assigned.surname }}
+          </button>
+        </span>
+        <span v-else>{{ item.assigned.time.toDate() | relativeTime }}</span>
+      </td>
+      <td>{{ item.updated.toDate() | relativeTime }}</td>
+      <td>{{ item.created.toDate() | dateFormat }}</td>
+    </template>
+  </List>
 </template>
 
 <script>
 import firebase from "@/firebase";
 const db = firebase.firestore();
-import componentMaker from "./shared.js";
+import { mapState } from "vuex";
+import moment from "moment";
+import List from "./List2";
 
-const component = componentMaker();
-const methods = {
-  assign(computer, user) {
-    const assignComputerToUser = firebase.functions().httpsCallable("assignComputerToUser");
-    return assignComputerToUser({ computer, user })
-    .then((result) => {
-      console.log(`assigned computer ${computer} to ${user}`);      
-    })
-    .catch((error) => {
-      console.log(error);
-      console.log(`assignComputerTouser(${computer}, ${user}) didn't work`); 
-    });
+export default {
+  components: { List },
+  data() {
+    return {
+      schema: {
+        computerName: {display: "Computer"},
+        osVersion: {display: "Windows"},
+        mfg: true,
+        model: true,
+        userGivenName: {display: "First"},
+        userSurname: {display: "Last"},
+        assigned: {sort:false },
+        updated: true,
+        created: true
+      },
+      collection: db.collection("Computers"),
+      items: db.collection("Computers"),
+    }
+  },
+  computed: {
+    ...mapState(["claims"]),
+    // Determine whether to show UI controls based on claims
+    hasPermission () {
+      return this.claims.hasOwnProperty("computers") &&
+        this.claims["computers"];
+    }
+  },
+  methods: {
+    assign(computer, user) {
+      const assignComputerToUser = firebase.functions().httpsCallable("assignComputerToUser");
+      return assignComputerToUser({ computer, user })
+      .then((result) => {
+        console.log(`assigned computer ${computer} to ${user}`);      
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(`assignComputerTouser(${computer}, ${user}) didn't work`); 
+      });
+    }
+  },
+  filters: {
+    dateFormat(date) {
+      return moment(date).format("YYYY MMM DD / HH:mm:ss");
+    },  
+    relativeTime(date) {
+      return moment(date).fromNow();
+    }
   }
 }
-
-Object.assign(component.methods, methods);
-
-component.created = function() {
-  this.$bind("items", db.collection("Computers"));
-}
-
-export default component;
 </script>
