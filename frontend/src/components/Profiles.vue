5666c8f92a2b5be89a9ba98082be2cc54b264700
Dean Stamler <dean@stamler.ca>
2019-06-10 16:42:17 -0400

    replace Profiles with Profiles2

--- frontend/src/components/Profiles.vue
+++ frontend/src/components/Profiles.vue
@@ -1,93 +1,69 @@
 <template>
-  <div id="container">
-    <!-- The Users mgmt UI is driven by Profiles rather than the auth users. Prior to 
-  performing actions the user should perform an update which will ensure the
-  profiles reflect the correct claims in firebase auth. The actual changes will 
-  be performed through a cloud function (multiple calls to chclaim) then an 
-  update will be called again (possibly only on the updated items based on 
-  update time?)-->
+  <div>
+    <!-- TODO: 
+    
+    Implement Entire Row as scoped slot rather than using Schema property?
+    https://jsfiddle.net/pyrello/odwag9mx/
 
-    <div id="taskArea">
-      <div v-if="taskAreaMode === 'default'">
-        <input type="textbox" placeholder="search..." v-model="search" />
+    -->
+    <List :select="true && hasPermission">
+      <template v-slot:taskAreaDefault="{ taskAreaMode, setTaskMode }">
         <button v-on:click="claimsToProfiles">🔄 Reload Profiles</button>
-        <button v-on:click="taskAreaMode='modClaims'" >✏️ Edit Claims</button>
-      </div>
-      <ModClaims v-if="taskAreaMode === 'modClaims'" v-on:cancel="taskAreaMode='default'" v-on:mod-claims="modClaims"/>
-    </div>
-
-    <table>
-      <tr>
-        <th>
-          <input type="checkbox" v-model="selectAll" v-on:click="toggleAll()">
-          {{ selected.length }}/{{ processedItems.length }} 
-        </th>
-        <th>user</th>
-        <th>email</th>
-        <th>claims</th>
-      </tr>
-      <tr v-for="item in processedItems" v-bind:key="item.id">
-        <td>
-          <input type="checkbox" v-bind:value="item.id" v-model="selected">
-        </td>
-        <td>{{ item.displayName }}</td>
-        <td>{{ item.email }}</td>
-        <td>{{ item.customClaims ? Object.keys(item.customClaims).join(", ") : "" }}</td>
-        <td v-if="item.onPremId">{{ item.onPremId }}</td>
-        <td v-else><button>Write AzureID to User doc</button></td>
-        <td><button>Revoke refresh tokens</button></td>
-        <!-- There are two IDs per user and we manually link them. 
-      The first is the Azure ObjectID. This is the UID in Auth users
-      and also the key in Profiles. 
-      The second is the userSourceAnchor for Azure sync, called
-      mS-DS-ConsistencyGuid in on-prem Active Directory. It is stored
-      in the Users document under the userSourceAnchor key. The above button
-      attempts to match the auth user/profile user to a Users user then if it
-      finds a single match it writes the Azure ObjectID to the Users doc -->
-      </tr>
-    </table>
+        <button v-on:click="setTaskMode('modClaims')" >✏️ Edit Claims</button>
+      </template>
+      <template v-slot:taskAreaNonDefault="{ taskAreaMode, setTaskMode }">
+        <ModClaims v-if="taskAreaMode === 'modClaims'" v-on:cancel="setTaskMode('default')" />
+      </template>
+      <template v-slot:lastCol="{ item }">
+        <button>Write AzureID to User doc</button>
+        <button>Revoke refresh tokens</button>
+      </template>
+    </List>
   </div>
 </template>
 
 <script>
 import firebase from "@/firebase";
 const db = firebase.firestore();
-import componentMaker from "./shared.js";
+import { mapState } from "vuex";
+import moment from "moment";
 import ModClaims from "@/components/ModClaims.vue";
+import List from "@/components/List.vue";
 
-const component = componentMaker();
-const methods = {
-  modClaims(data) {
-    const modClaims = firebase.functions().httpsCallable("modClaims");
-    modClaims({...data, users: this.selected})
-      .then((result) => {
-        // TODO: get and handle real responses from callable
-        console.log(JSON.stringify(result));
-      })
-      .catch((error) => {
-        // TODO: get and handle real responses from callable
-        console.log(error);
-      });
+export default {
+  components: { ModClaims, List },
+  data() {
+    return {
+      schema: {
+        displayName: {display:"user"},
+        email: true,
+        claims: {
+          derivation: obj => obj.customClaims ? Object.keys(obj.customClaims).join(", ") : ""
+        },
+      },
+      collection: db.collection("Profiles"),
+      items: db.collection("Profiles"),
+    }
   },
-  claimsToProfiles() {
-    const claimsToProfiles = firebase.functions().httpsCallable("claimsToProfiles");
-    claimsToProfiles({})
-      .then((result) => {
-        console.log(JSON.stringify(result));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  computed: {
+    ...mapState(["claims", "user"]),
+    // Determine whether to show UI controls based on claims
+    hasPermission () {
+      return this.claims.hasOwnProperty("profiles") &&
+        this.claims["profiles"];
+    }
+  },
+  methods: {
+    claimsToProfiles() {
+      const claimsToProfiles = firebase.functions().httpsCallable("claimsToProfiles");
+      claimsToProfiles({})
+        .then((result) => {
+          console.log(JSON.stringify(result));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   }
 }
-
-Object.assign(component.methods, methods);
-
-component.created = function() {
-  this.$bind("items", db.collection("Profiles"));
-}
-
-component.components = { ModClaims };
-
-export default component;
 </script>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -96,13 +96,6 @@ const router = new Router({
             import(/* webpackChunkName: "admin" */ "./components/Profiles.vue")
         },
         {
-          path: "profiles2",
-          name: "Profiles2",
-          meta: { claims: ["profiles", "audit"] },
-          component: () =>
-            import(/* webpackChunkName: "admin" */ "./components/Profiles2.vue")
-        },
-        {
           path: "computers",
           name: "Computers",
           meta: { claims: ["computers", "audit"] },
