8d2c15cbd8120938224e02640c77d7fdf27e2799
Dean Stamler <dean@stamler.ca>
2019-06-11 12:31:58 -0400

    code cleanup

--- frontend/src/components/Computers.vue
+++ frontend/src/components/Computers.vue
@@ -2,8 +2,10 @@
   <List>
     <template v-slot:columns="{ item }">
       <td>
-        <span v-if="!item.computerName.includes(item.serial)">❗({{ item.serial }})</span>
-        <br/>
+        <span v-if="!item.computerName.includes(item.serial)">
+          ❗({{ item.serial }})
+        </span>
+        <br />
         {{ item.computerName }}
       </td>
       <td>{{ item.osVersion }}</td>
@@ -14,18 +16,21 @@
       <td>
         <span v-if="!item.assigned">
           <!-- Show this if the device has no assignment -->
-          <button 
+          <button
             v-if="claims.computers === true"
-            v-on:click="assign(item.id, item.userSourceAnchor)">
+            v-on:click="assign(item.id, item.userSourceAnchor)"
+          >
             assign
           </button>
         </span>
-        <span v-else-if="item.assigned.userSourceAnchor !== item.userSourceAnchor">
+        <span
+          v-else-if="item.assigned.userSourceAnchor !== item.userSourceAnchor"
+        >
           <!-- Show this if the device has an assignment that doesn't
-          match the last user login -->
-          <button 
-            v-on:click="assign(item.id, item.userSourceAnchor)">
-            ❗assign, currently {{ item.assigned.givenName }} {{ item.assigned.surname }}
+          match the last user login-->
+          <button v-on:click="assign(item.id, item.userSourceAnchor)">
+            ❗assign, currently {{ item.assigned.givenName }}
+            {{ item.assigned.surname }}
           </button>
         </span>
         <span v-else>{{ item.assigned.time.toDate() | relativeTime }}</span>
@@ -48,39 +53,42 @@ export default {
   data() {
     return {
       schema: {
-        computerName: {display: "Computer"},
-        osVersion: {display: "Windows"},
+        computerName: { display: "Computer" },
+        osVersion: { display: "Windows" },
         mfg: true,
         model: true,
-        userGivenName: {display: "First"},
-        userSurname: {display: "Last"},
-        assigned: {sort:false },
+        userGivenName: { display: "First" },
+        userSurname: { display: "Last" },
+        assigned: { sort: false },
         updated: true,
         created: true
       },
       collection: db.collection("Computers"),
-      items: db.collection("Computers"),
-    }
+      items: db.collection("Computers")
+    };
   },
   computed: {
     ...mapState(["claims"]),
     // Determine whether to show UI controls based on claims
-    hasPermission () {
-      return this.claims.hasOwnProperty("computers") &&
-        this.claims["computers"];
+    hasPermission() {
+      return (
+        this.claims.hasOwnProperty("computers") && this.claims["computers"]
+      );
     }
   },
   methods: {
     assign(computer, user) {
-      const assignComputerToUser = firebase.functions().httpsCallable("assignComputerToUser");
+      const assignComputerToUser = firebase
+        .functions()
+        .httpsCallable("assignComputerToUser");
       return assignComputerToUser({ computer, user })
-      .then((result) => {
-        console.log(`assigned computer ${computer} to ${user}`);      
-      })
-      .catch((error) => {
-        console.log(error);
-        console.log(`assignComputerTouser(${computer}, ${user}) didn't work`); 
-      });
+        .then(() => {
+          console.log(`assigned computer ${computer} to ${user}`);
+        })
+        .catch(error => {
+          console.log(error);
+          console.log(`assignComputerTouser(${computer}, ${user}) didn't work`);
+        });
     }
   },
   filters: {
@@ -91,5 +99,5 @@ export default {
       return moment(date).fromNow();
     }
   }
-}
+};
 </script>
