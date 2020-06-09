aff869260d5bc1a1927fe0ce6070f3f9185cb7d2
Dean Stamler <dean@stamler.ca>
2020-06-09 12:34:24 -0400

    add projects.js (untested) and State Diagram

---
+++ backend/functions/projects.js
@@ -0,0 +1,18 @@
+/* 
+
+This module exports callable handlers (functions.https.onCall(<handler_func>))
+https://firebase.google.com/docs/functions/callable
+
+*/
+
+const callableIsAuthorized = require('./utilities.js').callableIsAuthorized;
+
+
+
+// The claimsHandler writes a searchStrings array to one or more provided 
+// documents containing the tokenized set of words in the provided properties
+exports.documentPropertiesToSearchArray = async (data, context, db) => {
+    callableIsAuthorized(context, ['projects'], validate, data);
+    const project = db.collection("Projects").doc(data.projectId);
+    return project.update({});
+};
