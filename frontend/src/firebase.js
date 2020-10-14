68bf066187547cae882cdb38af39048b938975ac
Dean Stamler <dean@stamler.ca>
2020-10-14 13:24:22 -0400

    point to emulator with on localhost

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -13,4 +13,12 @@ firebase.initializeApp({
   messagingSenderId: "1033101603088"
 });
 
+const db = firebase.firestore();
+if (location.hostname === "localhost") {
+  db.settings({
+    host: "localhost:8080",
+    ssl: false
+  });
+}
+
 export default firebase;
