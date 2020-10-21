c10dab6f489cb0e2db555971dd2b7d28c91ce91d
Dean Stamler <dean@stamler.ca>
2020-10-21 12:31:37 -0400

    delete unused code

--- frontend/src/components/Profiles.vue
+++ frontend/src/components/Profiles.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <List :select="true && hasPermission">
-      <template v-slot:taskAreaDefault="{ taskAreaMode, setTaskMode }">
-        <button v-on:click="claimsToProfiles">🔄 Reload Profiles</button>
-        <button v-on:click="setTaskMode('modClaims')">✏️ Edit Claims</button>
-      </template>
-      <template v-slot:taskAreaNonDefault="{ taskAreaMode, setTaskMode }">
-        <ModClaims
-          v-if="taskAreaMode === 'modClaims'"
-          v-on:cancel="setTaskMode('default')"
-        />
-      </template>
-      <template v-slot:columns="{ item }">
-        <td>{{ item.displayName }}</td>
-        <td>{{ item.email }}</td>
-        <td>{{ item.customClaims | keysString }}</td>
-        <button>Write AzureID to User doc</button>
-        <button>Revoke refresh tokens</button>
-      </template>
-    </List>
+    <List />
   </div>
 </template>
 
@@ -26,47 +8,18 @@
 import firebase from "@/firebase";
 const db = firebase.firestore();
 import { mapState } from "vuex";
-import ModClaims from "@/components/ModClaims.vue";
 import List from "@/components/ProfilesList.vue";
 
 export default {
-  components: { ModClaims, List },
+  components: { List },
   data() {
     return {
-      schema: {
-        displayName: { display: "user" },
-        email: true,
-        claims: true
-      },
       collection: db.collection("Profiles"),
       items: db.collection("Profiles")
     };
   },
   computed: {
-    ...mapState(["claims", "user"]),
-    // Determine whether to show UI controls based on claims
-    hasPermission() {
-      return this.claims.hasOwnProperty("profiles") && this.claims["profiles"];
-    }
-  },
-  methods: {
-    claimsToProfiles() {
-      const claimsToProfiles = firebase
-        .functions()
-        .httpsCallable("claimsToProfiles");
-      claimsToProfiles({})
-        .then(result => {
-          console.log(JSON.stringify(result));
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  },
-  filters: {
-    keysString(obj) {
-      return obj ? Object.keys(obj).join(", ") : "";
-    }
+    ...mapState(["claims", "user"])
   }
 };
 </script>
