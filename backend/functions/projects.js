2da5c3cffbf8dba8529d393b30f5434ef6d05332
Dean Stamler <dean@stamler.ca>
2020-11-03 13:15:39 -0500

    prettier formatting

--- backend/functions/projects.js
+++ backend/functions/projects.js
@@ -5,14 +5,12 @@ https://firebase.google.com/docs/functions/callable
 
 */
 
-const callableIsAuthorized = require('./utilities.js').callableIsAuthorized;
+const callableIsAuthorized = require("./utilities.js").callableIsAuthorized;
 
-
-
-// The claimsHandler writes a searchStrings array to one or more provided 
+// The claimsHandler writes a searchStrings array to one or more provided
 // documents containing the tokenized set of words in the provided properties
 exports.documentPropertiesToSearchArray = async (data, context, db) => {
-    callableIsAuthorized(context, ['projects'], validate, data);
-    const project = db.collection("Projects").doc(data.projectId);
-    return project.update({});
+  callableIsAuthorized(context, ["projects"], validate, data);
+  const project = db.collection("Projects").doc(data.projectId);
+  return project.update({});
 };
--- backend/functions/rawLogins.js
+++ backend/functions/rawLogins.js
@@ -1,95 +1,92 @@
-const serverTimestamp = require('firebase-admin').firestore.FieldValue.serverTimestamp
-const makeSlug = require('./utilities.js').makeSlug;
-const callableIsAuthorized = require('./utilities.js').callableIsAuthorized;
-const Ajv = require('ajv')
-const schema = require('./RawLogins.schema.json')
-const functions = require('firebase-functions');
-const _ = require('lodash');
+const serverTimestamp = require("firebase-admin").firestore.FieldValue
+  .serverTimestamp;
+const makeSlug = require("./utilities.js").makeSlug;
+const callableIsAuthorized = require("./utilities.js").callableIsAuthorized;
+const Ajv = require("ajv");
+const schema = require("./RawLogins.schema.json");
+const functions = require("firebase-functions");
+const _ = require("lodash");
 
 const ajv = new Ajv({
   removeAdditional: true,
   coerceTypes: true,
-  allErrors: true // required for 'removeIfFails' keyword
+  allErrors: true, // required for 'removeIfFails' keyword
 });
 
-const ajv_vanilla = new Ajv()
+const ajv_vanilla = new Ajv();
 const validateCleanup = ajv_vanilla.compile({
-  "type": "object",
-  "additionalProperties": false,
-  "properties": {
-    "computerName": {
-      "$id": "#/properties/computerName",
-      "type": "string",
-      "title": "Computer name",
-      "examples": [
-        "MHE-41KE281PH"
-      ],
-      "minLength": 1,
-      "maxLength": 48,
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    computerName: {
+      $id: "#/properties/computerName",
+      type: "string",
+      title: "Computer name",
+      examples: ["MHE-41KE281PH"],
+      minLength: 1,
+      maxLength: 48,
     },
-
-  }
- });
+  },
+});
 
 // The 'removeIfFails' keyword deletes a property if it is invalid. Created to
-// remove email properties that don't validate, i.e. empty strings. 
+// remove email properties that don't validate, i.e. empty strings.
 // See https://github.com/epoberezkin/ajv/issues/300
 // Uses inline validator since aggregated errors cannot be accessed otherwise
 // https://github.com/epoberezkin/ajv/issues/208#issuecomment-225445407
 
-ajv.addKeyword('removeIfFails', {
+ajv.addKeyword("removeIfFails", {
   inline: function (it, keyword) {
     // verify that removeIfFails is set to 'true';
     if (it.schema.removeIfFails === true) {
-      // TODO replace errs_ and errs__ with errors and vErrors and 
+      // TODO replace errs_ and errs__ with errors and vErrors and
       // find the error(s) that match the current schema item
       return `if (errors > 0) {
         // only keep errors whose dataPaths don't match this dataPath
         vErrors = vErrors.filter(
-          e => e.dataPath !== ('.' + ${
-            it.dataPathArr[it.dataLevel]
-          }));
+          e => e.dataPath !== ('.' + ${it.dataPathArr[it.dataLevel]}));
         errors = vErrors.length;
 
         // delete the failing element
-        delete data${(it.dataLevel - 1 || '')}[${it.dataPathArr[it.dataLevel]}];
+        delete data${it.dataLevel - 1 || ""}[${it.dataPathArr[it.dataLevel]}];
       }`;
     }
     return ``;
     // TODO: test validation-time code and branching
   },
-  metaSchema: { type: 'boolean' },
+  metaSchema: { type: "boolean" },
   statements: true, // allow side-effects and modification
   valid: true,
-  modifying: true
-})
+  modifying: true,
+});
 
 const validate = ajv.compile(schema);
 
 exports.handler = async (req, res, db) => {
-
   // Validate the secret sent in the header from the client.
   const appSecret = functions.config().tybalt.radiator.secret;
   if (appSecret !== undefined) {
-    const authHeader = req.get('Authorization');
+    const authHeader = req.get("Authorization");
 
     let reqSecret = null;
     if (authHeader !== undefined) {
-      reqSecret = authHeader.replace('TYBALT ','').trim();
+      reqSecret = authHeader.replace("TYBALT ", "").trim();
     }
-    if (reqSecret !== appSecret ) {
-      console.log(`${reqSecret} from ${authHeader} doesn't match expected ${appSecret}`);
+    if (reqSecret !== appSecret) {
+      console.log(
+        `${reqSecret} from ${authHeader} doesn't match expected ${appSecret}`
+      );
       return res.status(401).send();
     }
   }
 
   // req.body can be used directly as JSON if this passes
-  if (req.get('Content-Type') !== "application/json") {
+  if (req.get("Content-Type") !== "application/json") {
     return res.status(415).send();
   }
 
-  if (req.method !== 'POST') {
-    res.header('Allow', 'POST');
+  if (req.method !== "POST") {
+    res.header("Allow", "POST");
     return res.status(405).send();
   }
 
@@ -105,7 +102,6 @@ exports.handler = async (req, res, db) => {
     const valid = validate(d);
   */
 
-
   try {
     if (!valid) {
       // Invalid submission, store RawLogin for later processing
@@ -115,7 +111,7 @@ exports.handler = async (req, res, db) => {
         console.log("empty submission, not saving");
         return res.status(400).send();
       } else {
-        await db.collection('RawLogins').doc().set(d);
+        await db.collection("RawLogins").doc().set(d);
       }
     } else {
       // write valid object to database
@@ -126,12 +122,12 @@ exports.handler = async (req, res, db) => {
     console.log(error.message);
     return res.status(500).send();
   }
-}
+};
 
 // Creates or updates Computers and Users document, creates Logins document
 async function storeValidLogin(d, db) {
-  const slug = makeSlug(d.serial, d.mfg) // key for Computers collection
-  const computerRef = db.collection('Computers').doc(slug)
+  const slug = makeSlug(d.serial, d.mfg); // key for Computers collection
+  const computerRef = db.collection("Computers").doc(slug);
   let userRef;
   try {
     // try to match existing user, otherwise make a new one. If database
@@ -139,15 +135,15 @@ async function storeValidLogin(d, db) {
     userRef = await getUserRef(d, db);
   } catch (error) {
     d.error = error.message;
-    return db.collection('RawLogins').doc().set(d);
+    return db.collection("RawLogins").doc().set(d);
   }
 
   // Start a write batch
   var batch = db.batch();
 
-  d.updated = serverTimestamp()
+  d.updated = serverTimestamp();
   batch.set(computerRef, d, {
-    merge: true
+    merge: true,
   });
 
   userObject = {
@@ -156,18 +152,18 @@ async function storeValidLogin(d, db) {
     surname: d.userSurname,
     lastComputer: slug,
     updated: serverTimestamp(),
-    userSourceAnchor: d.userSourceAnchor.toLowerCase()
+    userSourceAnchor: d.userSourceAnchor.toLowerCase(),
   };
 
   // Confirm optional email prop exists before calling .toLowerCase()
   if (d.email) {
-    userObject.email = d.email.toLowerCase()
+    userObject.email = d.email.toLowerCase();
   }
   // TODO: Check if User has azureObjectID. If it doesn't,
   // try to match it with auth() users by upn/email (Soft match) and then
   // write the key to azureObjectID property
   batch.set(userRef, userObject, {
-    merge: true
+    merge: true,
   });
 
   // Create new Login document
@@ -175,9 +171,9 @@ async function storeValidLogin(d, db) {
     userSourceAnchor: d.userSourceAnchor.toLowerCase(),
     givenName: d.userGivenName,
     surname: d.userSurname,
-    computer: slug
+    computer: slug,
   };
-  batch.set(db.collection('Logins').doc(), loginObject);
+  batch.set(db.collection("Logins").doc(), loginObject);
 
   // Commit the batch which returns an array of WriteResults
   return batch.commit();
@@ -186,18 +182,20 @@ async function storeValidLogin(d, db) {
 async function getUserRef(d, db) {
   // If I user was deleted from the directory then recreated we don't want to
   // represent them twice in the database. We reuse the same user entry and
-  // keep the previous ID/Document. 
+  // keep the previous ID/Document.
   // NB THIS REQUIRES THAT WE STORE d.userSourceAnchor in Users documents
-  // The situation where a document exists with a key that matches another 
-  // document's userSourceAnchor property is problematic and needs to be 
+  // The situation where a document exists with a key that matches another
+  // document's userSourceAnchor property is problematic and needs to be
   // handled in this code. Essentially the key should only coincidentally match
   // a userSourceAnchor
 
-  const usersRef = db.collection('Users');
+  const usersRef = db.collection("Users");
   for (let prop of ["userSourceAnchor", "upn", "email"]) {
     // skip a property if it doesn't exist in d
-    if ( d[prop] === null || d[prop] === undefined ) { continue; }
-    
+    if (d[prop] === null || d[prop] === undefined) {
+      continue;
+    }
+
     // query for a user with matching prop
     // eslint-disable-next-line no-await-in-loop
     let result = await usersRef.where(prop, "==", d[prop].toLowerCase()).get();
@@ -215,7 +213,7 @@ async function getUserRef(d, db) {
 
   // if zero results are returned, return a ref to a new document
   // otherwise we'll overwrite an existing user.
-  return db.collection('Users').doc();
+  return db.collection("Users").doc();
 }
 
 // This function deletes all rawLogins except for the latest
@@ -225,35 +223,41 @@ async function getUserRef(d, db) {
 exports.cleanup = async (data, context, db) => {
   // unComment this line if calling from client rather than onCreate()
   //callableIsAuthorized(context, ['admin'], validateCleanup, data);
-  console.log(`cleanup ${data.computerName}`)
+  console.log(`cleanup ${data.computerName}`);
   // Get the latest rawLogin with specified computerName
-  let latest_item_snapshot = await db.collection("RawLogins")
+  let latest_item_snapshot = await db
+    .collection("RawLogins")
     .where("computerName", "==", data.computerName)
-    .orderBy("created","desc")
+    .orderBy("created", "desc")
     .limit(1)
     .get();
-  
+
   if (latest_item_snapshot.empty) {
-    throw new functions.https.HttpsError("not-found",
-    "The provided computerName doesn't exist");
+    throw new functions.https.HttpsError(
+      "not-found",
+      "The provided computerName doesn't exist"
+    );
   }
 
   // Get previous rawLogin with specified computerName for deletion
-  let old_items_snapshot = await db.collection("RawLogins")
+  let old_items_snapshot = await db
+    .collection("RawLogins")
     .where("computerName", "==", data.computerName)
     .where("created", "<", latest_item_snapshot.docs[0].data().created)
-    .orderBy("created","desc")
+    .orderBy("created", "desc")
     .get();
 
   if (old_items_snapshot.size > 0) {
     // Do the deletion here
     const batch = db.batch();
-    old_items_snapshot.forEach(element => {
+    old_items_snapshot.forEach((element) => {
       batch.delete(element.ref);
     });
     try {
       await batch.commit();
-      return `Deleted ${old_items_snapshot.size} entries of ${data.computerName} prior to ${latest_item_snapshot.docs[0].data().created.toDate()}`
+      return `Deleted ${old_items_snapshot.size} entries of ${
+        data.computerName
+      } prior to ${latest_item_snapshot.docs[0].data().created.toDate()}`;
     } catch (error) {
       return error.message;
     }
