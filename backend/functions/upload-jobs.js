7aab62b4983d7f5df3a65f8be9f2b8690e075aac
Dean Stamler <dean@stamler.ca>
2020-11-05 13:27:09 -0500

    create upload-jobs.js

---
+++ backend/functions/upload-jobs.js
@@ -0,0 +1,57 @@
+// import data from node CLI
+
+const admin = require("firebase-admin");
+const serviceAccount = require("../../../../Downloads/serviceAccountKey.json");
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+async function writeJobs(filename, jobtype, collection) {
+  console.log("Writing jobs to firestore...");
+  // load the file and parse it
+  const fs = require("fs");
+  const json = JSON.parse(fs.readFileSync(filename));
+  const db = admin.firestore();
+
+  for (const item of json) {
+    // check if the proposal or project name matches corresponding regex
+    // and do any other validation, log and skip if it fails
+    const proposalRegex = /^P[0-9]{2}-[0-9]{3,}(-[0-9]+)?$/;
+    const projectRegex = /^[0-9]{2}-[0-9]{3,}(-[0-9]+)?$/;
+    const regex = jobtype === "proposal" ? proposalRegex : projectRegex;
+    if (!regex.test(item.id)) {
+      console.log(`job ${item.id} isn't valid, skipping...`);
+      continue;
+    }
+    if (jobtype === "project" && item.proposal !== undefined) {
+      if (!proposalRegex.test(item.proposal)) {
+        console.log(`invalid proposal format for job ${item.id}, skipping...`);
+        continue;
+      }
+    }
+
+    const id = item.id.slice();
+    delete item.id;
+    await db.collection(collection).doc(id).set(item);
+    console.log(`wrote ${id} to firestore`);
+  }
+}
+
+let collection = "Jobs";
+const filename = process.argv[2];
+const jobtype = process.argv[3];
+if (process.argv.length === 5) {
+  collection = process.argv[4];
+} else if (process.argv.length !== 4) {
+  console.log(
+    "Usage: \nnode upload-jobs.js <filename> <project/proposal> [<collection>]\n"
+  );
+  process.exit(1);
+} else {
+  writeJobs(filename, jobtype, collection)
+    .then((result) => {
+      return Promise.resolve(process.exit(0));
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
