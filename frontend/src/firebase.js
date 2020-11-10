8839375684849c8c7f0ffbf86937a172410fef1c
Dean Stamler <dean@stamler.ca>
2020-11-10 00:34:12 -0500

    add missing dependency

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "@firebase/app";
 import "@firebase/auth";
 import "@firebase/functions";
 import "@firebase/firestore";
+import "@firebase/storage";
 
 // Initialize Firebase
 firebase.initializeApp({
