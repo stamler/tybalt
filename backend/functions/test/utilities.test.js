7fe85d5eb74fb4ca25457a9c2f6285e352184166
Dean Stamler <dean@stamler.ca>
2020-11-11 12:24:39 -0500

    formatting

--- backend/functions/test/utilities.test.js
+++ backend/functions/test/utilities.test.js
@@ -1,52 +1,72 @@
-const assert = require('chai').assert;
+const assert = require("chai").assert;
 
 describe("utilities module", () => {
-  const utilitiesModule = require('../utilities.js')
+  const utilitiesModule = require("../utilities.js");
   describe("makeSlug()", () => {
     // Test setup for makeSlug() in utilities.js
     const makeSlug = utilitiesModule.makeSlug;
     it("makes a slug from serial number and manufactuer strings", () => {
-      assert.strictEqual(makeSlug(" QKF3421 "," Smell Computer, Corporation, Inc.   "), "QKF3421,smell_computer_corporation");
-      assert.strictEqual(makeSlug(" QK F3421 ","Award Software International, Inc."), "QKF3421,award_software_international");
-      assert.strictEqual(makeSlug(" QKF34 21 ","Phoenix Techno, LTD"), "QKF3421,phoenix_techno");
-      assert.strictEqual(makeSlug(" Q-KF3421 ","Dell Inc.  "), "Q-KF3421,dell");
-      assert.strictEqual(makeSlug("QKF34,21","Dell Inc.     "), "QKF3421,dell");
-      assert.strictEqual(makeSlug("QK/F34,21","Dell/Inc.     "), "QKF3421,dell");
+      assert.strictEqual(
+        makeSlug(" QKF3421 ", " Smell Computer, Corporation, Inc.   "),
+        "QKF3421,smell_computer_corporation"
+      );
+      assert.strictEqual(
+        makeSlug(" QK F3421 ", "Award Software International, Inc."),
+        "QKF3421,award_software_international"
+      );
+      assert.strictEqual(
+        makeSlug(" QKF34 21 ", "Phoenix Techno, LTD"),
+        "QKF3421,phoenix_techno"
+      );
+      assert.strictEqual(
+        makeSlug(" Q-KF3421 ", "Dell Inc.  "),
+        "Q-KF3421,dell"
+      );
+      assert.strictEqual(
+        makeSlug("QKF34,21", "Dell Inc.     "),
+        "QKF3421,dell"
+      );
+      assert.strictEqual(
+        makeSlug("QK/F34,21", "Dell/Inc.     "),
+        "QKF3421,dell"
+      );
     });
     it("throws error if serial or mfg components are too short", () => {
-      assert.throws(() => { makeSlug(" P1E  ", " J. inc") }, Error);
+      assert.throws(() => {
+        makeSlug(" P1E  ", " J. inc");
+      }, Error);
     });
   });
 
   describe("callableIsAuthorized()", () => {
     // Test setup for makeSlug() in utilities.js
-    const Ajv = require('ajv')
-    const ajv = new Ajv()
+    const Ajv = require("ajv");
+    const ajv = new Ajv();
 
     const callableIsAuthorized = utilitiesModule.callableIsAuthorized;
-    const ctxAdmin = {auth: { token: { admin: true}}};
+    const ctxAdmin = { auth: { token: { admin: true } } };
     const validateAllFn = ajv.compile(true);
     const validateNoneFn = ajv.compile(false);
     it("returns true if data validates & context has a required claim", () => {
       assert.strictEqual(
-        callableIsAuthorized(ctxAdmin,['admin'], validateAllFn, "test"),
+        callableIsAuthorized(ctxAdmin, ["admin"], validateAllFn, "test"),
         true
       );
     });
     it("throws if the context isn't authenticated", () => {
       assert.throws(() => {
-        callableIsAuthorized({},['admin'], validateAllFn, "test")
+        callableIsAuthorized({}, ["admin"], validateAllFn, "test");
       });
     });
     it("throws if the context doesn't have a matching claim", () => {
       assert.throws(() => {
-        callableIsAuthorized(ctxAdmin,['foo'], validateAllFn, "test")
+        callableIsAuthorized(ctxAdmin, ["foo"], validateAllFn, "test");
       });
     });
     it("throws if data doesn't validate", () => {
       assert.throws(() => {
-        callableIsAuthorized(ctxAdmin,['admin'], validateNoneFn, "test")
+        callableIsAuthorized(ctxAdmin, ["admin"], validateNoneFn, "test");
       });
     });
   });
-})
+});
