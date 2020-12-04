f9332cf6b9033ff2a1d6b1a431b4668c080619f3
Dean Stamler <dean@stamler.ca>
2020-12-03 23:03:19 -0500

    TypeScript migration

--- backend/functions/src/chclaim.ts
+++ backend/functions/src/chclaim.ts
@@ -1,10 +1,11 @@
 // add or remove custom claims to user from node CLI
+// npx ts-node chclaim.ts <action> <email> [<claim>]
 
 const admin = require("firebase-admin");
 const serviceAccount = require("../../../../../Downloads/serviceAccountKey.json");
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-async function chclaim(action, email, claim) {
+async function chclaim(action: string, email: string, claim: string | undefined) {
   const user = await admin.auth().getUserByEmail(email);
   const claims = user.customClaims || {}; // preserve existing claims
   if (action !== "add" && action !== "remove" && action !== "view") {
@@ -15,7 +16,7 @@ async function chclaim(action, email, claim) {
   if (action === "view") {
     return Promise.resolve(claims);
   }
-  if (claims && claims[claim] === true) {
+  if (claims && claim && claims[claim] === true) {
     if (action === "remove") {
       delete claims[claim];
       return admin.auth().setCustomUserClaims(user.uid, claims);
@@ -27,21 +28,27 @@ async function chclaim(action, email, claim) {
   }
   if (action === "remove") {
     return Promise.reject(
-      new Error(`${user.displayName} missing claim ${claim}`)
+      new Error(`${user.displayName} missing claim ${claim} or no claim provided`)
     );
   } else if (action === "add") {
-    claims[claim] = true;
+    if (claim) {
+      claims[claim] = true;
+    } else {
+      return Promise.reject(
+        new Error("No claim was provided to add command")
+      );
+    }
     return admin.auth().setCustomUserClaims(user.uid, claims);
   }
   return Promise.reject(new Error("Other shit went down"));
 }
 
-let claimArg = null;
+let claimArg: string | undefined;
 if (process.argv.length === 5) {
   claimArg = process.argv[4];
 } else if (process.argv.length !== 4) {
   console.log(
-    "Usage: \nnode chclaim.js add <email> <claim>\nnode chclaim.js remove <email> <claim>\nnode chclaim.js view <email>"
+    "Usage: \nts-node chclaim.js add <email> <claim>\nts-node chclaim.js remove <email> <claim>\nts-node chclaim.js view <email>"
   );
   process.exit(1);
 }
@@ -52,7 +59,7 @@ const emailArg = process.argv[3];
 chclaim(actionArg, emailArg, claimArg)
   .then((result) => {
     if (actionArg === "view") {
-      for (key in result) {
+      for (const key in result) {
         console.log(`${key}: ${result[key]}`);
       }
       return Promise.resolve(process.exit(0));
@@ -62,6 +69,6 @@ chclaim(actionArg, emailArg, claimArg)
     }
   })
   .catch((err) => {
-    console.log(`Did not ${actionArg} claim ${claim}: ` + err);
+    console.log(`Did not ${actionArg} claim ${claimArg}: ` + err);
     process.exit(1);
   });
