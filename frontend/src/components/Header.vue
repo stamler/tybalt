eab69bf1238ec9414c9bff5c9512d289c2f9da5e
Dean Stamler <dean@stamler.ca>
2021-02-02 10:31:52 -0500

    Add help link to documentation

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="header">
     <div class="linksstart">
-      <router-link to="#" v-on:click.native="toggleMenu">
-        &#9776;
-      </router-link>
+      <router-link to="#" v-on:click.native="toggleMenu"> &#9776; </router-link>
     </div>
     <div class="linksend">
       <router-link to="/me">{{ user.displayName }}</router-link>
+      <a
+        href="https://github.com/tbt-eng/docs/blob/master/tybalt.md"
+        target="_blank"
+        >Help</a
+      >
     </div>
   </div>
 </template>
