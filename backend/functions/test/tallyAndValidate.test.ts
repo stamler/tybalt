f880f9cee5bd8d1e3e61cff63b94eaff96d40040
Dean Stamler <dean@stamler.ca>
2021-04-21 11:54:48 -0400

    write tests

--- backend/functions/test/tallyAndValidate.test.ts
+++ backend/functions/test/tallyAndValidate.test.ts
@@ -31,41 +31,108 @@ describe("tallyAndValidate", async () => {
   const workDescription = "Generic Description";
   const fullITDay = { division: "CI", divisionName: "Information Technology", timetype: "R", timetypeName: "Hours Worked", hours: 8, workDescription };
 
-  beforeEach("reset data", async () => {
-    await cleanupFirestore(projectId);
-    await db.collection("TimeEntries").add({ date: new Date(2020,0,4), uid: alice.uid, weekEnding, ...fullITDay });
-    await db.collection("TimeEntries").add({ date: new Date(2020,0,5), uid: alice.uid, weekEnding, ...fullITDay });
-    await db.collection("TimeEntries").add({ date: new Date(2020,0,6), uid: alice.uid, weekEnding, ...fullITDay });
-    await db.collection("TimeEntries").add({ date: new Date(2020,0,7), uid: alice.uid, weekEnding, ...fullITDay });
-    await db.collection("TimeEntries").add({ date: new Date(2020,0,8), uid: alice.uid, weekEnding, ...fullITDay });
-  });
-
-  describe("administrative timesheet with no job numbers or chargeable time", () => {
+  describe("administrative timesheet with no job numbers or chargeable time", async () => {
+    beforeEach("reset data", async () => {
+      await cleanupFirestore(projectId);
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,4), uid: alice.uid, weekEnding, ...fullITDay });
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,5), uid: alice.uid, weekEnding, ...fullITDay });
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,6), uid: alice.uid, weekEnding, ...fullITDay });
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,7), uid: alice.uid, weekEnding, ...fullITDay });
+    });  
 
     it("tallies and validates for salaried staff member with 40 regular hours", async () => {
-      const timeEntries00 = await db
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,8), uid: alice.uid, weekEnding, ...fullITDay });
+      const timeEntries = await db
         .collection("TimeEntries")
         .where("uid", "==", alice.uid)
         .where("weekEnding", "==", weekEnding)
         .orderBy("date", "asc")
         .get();
-      assert.equal(timeEntries00.size,5);
-      const tally = await tallyAndValidate(auth, profile, timeEntries00, weekEnding);
-      assert.equal(tally.workHoursTally.hours,40);
-      assert.equal(tally.workHoursTally.jobHours,0);
-      assert.equal(tally.workHoursTally.noJobNumber,40);
-    });    
+      assert.equal(timeEntries.size,5);
+      const tally = await tallyAndValidate(auth, profile, timeEntries, weekEnding);
+      assert.equal(tally.workHoursTally.hours,40,"hours tally doesn't match");
+      assert.equal(tally.workHoursTally.jobHours,0, "jobHours tally doesn't match");
+      assert.equal(tally.workHoursTally.noJobNumber,40, "noJobNumber tally doesn't match");
+    });
     it("rejects for salaried staff member with fewer than 40 regular hours", async () => {
-      const timeEntries01 = await db
+      const timeEntries = await db
         .collection("TimeEntries")
         .where("uid", "==", alice.uid)
         .where("weekEnding", "==", weekEnding)
         .orderBy("date", "asc")
-        .limit(4)
         .get();
 
-      assert.equal(timeEntries01.size,4);
-      await assert.isRejected(tallyAndValidate(auth, profile, timeEntries01, weekEnding))
+      assert.equal(timeEntries.size,4);
+      await assert.isRejected(
+        tallyAndValidate(auth, profile, timeEntries, weekEnding),
+        "Salaried staff must have a minimum of 40 hours on each time sheet."
+      );
+    });    
+    it("rejects for salaried or hourly staff member with missing tbtePayrollId", async () => {
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,8), uid: alice.uid, weekEnding, ...fullITDay });
+      const timeEntries = await db
+        .collection("TimeEntries")
+        .where("uid", "==", alice.uid)
+        .where("weekEnding", "==", weekEnding)
+        .orderBy("date", "asc")
+        .get();
+
+      assert.equal(timeEntries.size,5);
+      const profileB = tester.firestore.makeDocumentSnapshot({ ...alice, salary: false, managerUid: "bob", managerName: "Bob Example"},"Profiles/alice");
+      const profileC = tester.firestore.makeDocumentSnapshot({ ...alice, salary: true, managerUid: "bob", managerName: "Bob Example"},"Profiles/alice");
+      await assert.isRejected(
+        tallyAndValidate(auth, profileB, timeEntries, weekEnding),
+        "The Profile for this user doesn't contain a tbtePayrollId"
+      );
+      await assert.isRejected(
+        tallyAndValidate(auth, profileC, timeEntries, weekEnding),
+        "The Profile for this user doesn't contain a tbtePayrollId"
+      );
+    });    
+    it("tallies and validates for hourly staff member with fewer than 40 regular hours", async () => {
+      const timeEntries = await db
+        .collection("TimeEntries")
+        .where("uid", "==", alice.uid)
+        .where("weekEnding", "==", weekEnding)
+        .orderBy("date", "asc")
+        .get();
+
+      const profileB = tester.firestore.makeDocumentSnapshot({ ...alice, salary: false, managerUid: "bob", managerName: "Bob Example", tbtePayrollId: 28},"Profiles/alice");
+      assert.equal(timeEntries.size,4);
+      const tally = await tallyAndValidate(auth, profileB, timeEntries, weekEnding);
+      assert.equal(tally.workHoursTally.hours,32,"hours tally doesn't match");
+      assert.equal(tally.workHoursTally.jobHours,0, "jobHours tally doesn't match");
+      assert.equal(tally.workHoursTally.noJobNumber,32, "noJobNumber tally doesn't match");
+    });    
+    it("rejects for salaried staff member if PPTO pushes hours over 40", async () => {
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,8), uid: alice.uid, weekEnding, timetype: "OP", timetypeName: "PPTO", hours: 9 });
+      const timeEntries = await db
+        .collection("TimeEntries")
+        .where("uid", "==", alice.uid)
+        .where("weekEnding", "==", weekEnding)
+        .orderBy("date", "asc")
+        .get();
+
+      assert.equal(timeEntries.size,5);
+      await assert.isRejected(
+        tallyAndValidate(auth, profile, timeEntries, weekEnding),
+        "Salaried staff cannot claim Vacation or PPTO entries that increase total hours beyond 40."
+      );
+    });    
+    it("rejects for salaried staff member if Vacation pushes hours over 40", async () => {
+      await db.collection("TimeEntries").add({ date: new Date(2020,0,8), uid: alice.uid, weekEnding, timetype: "OV", timetypeName: "Vacation", hours: 9 });
+      const timeEntries = await db
+        .collection("TimeEntries")
+        .where("uid", "==", alice.uid)
+        .where("weekEnding", "==", weekEnding)
+        .orderBy("date", "asc")
+        .get();
+
+      assert.equal(timeEntries.size,5);
+      await assert.isRejected(
+        tallyAndValidate(auth, profile, timeEntries, weekEnding),
+        "Salaried staff cannot claim Vacation or PPTO entries that increase total hours beyond 40."
+      );
     });    
   });
 });
