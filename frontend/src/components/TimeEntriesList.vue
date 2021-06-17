17e4920e939bd2aafffb8577edf3f678bc5db425
Dean Stamler <dean@stamler.ca>
2021-06-17 08:40:51 -0400

    change display of workrecord in UI

--- frontend/src/components/TimeEntriesList.vue
+++ frontend/src/components/TimeEntriesList.vue
@@ -54,7 +54,7 @@
           </div>
           <div v-if="item.workDescription" class="thirdline">
             <span v-if="item.workrecord !== undefined">
-              workrecord: {{ item.workrecord }} /
+              Work Record: {{ item.workrecord }} /
             </span>
             {{ item.workDescription }}
           </div>
