77d81fe8ebee11ff0088c26deb02f8a33c56ceeb
Dean Stamler <dev@stamler.ca>
2023-02-17 13:41:50 -0500

    move firebase initialization to config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -4,7 +4,7 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 import "firebase/compat/analytics";
-import { APP_HOSTNAME } from "./config";
+import { FIREBASE_CONFIG } from "./config";
 // Initialize Firebase
 
 // Dynamic State Partitioning breaks the login flow and causes an endless loop
@@ -12,16 +12,7 @@ import { APP_HOSTNAME } from "./config";
 // authDomain is set to match the actual custom URL of the site, replacing
 // firebaseApp-ca99XX.firebaseapp.com with APP_HOSTNAME and the Azure App
 // registration configuration matches.
-export const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCZpTxn-kK2zEpG7rlXn_eGsFHa4xmuVPM",
-  authDomain: APP_HOSTNAME, // include https://APP_HOSTNAME/__/auth/handler in Azure App registration "Redirect URIs"
-  databaseURL: "https://charade-ca63f.firebaseio.com",
-  projectId: "charade-ca63f",
-  storageBucket: "charade-ca63f.appspot.com",
-  messagingSenderId: "1033101603088",
-  appId: "1:1033101603088:web:805b8fd493e12cd44db595",
-  measurementId: "G-Y8GRRSDLZD",
-});
+export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
 
 export const analytics = firebase.analytics();
 
