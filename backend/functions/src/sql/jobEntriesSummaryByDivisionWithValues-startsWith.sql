4e3c5a776f25b641f5634f2b3539a978a931dca1
Dean Stamler <dev@stamler.ca>
2023-07-01 12:48:28 -0400

    add total and %

--- backend/functions/src/sql/jobEntriesSummaryByDivisionWithValues-startsWith.sql
+++ backend/functions/src/sql/jobEntriesSummaryByDivisionWithValues-startsWith.sql
@@ -1,12 +1,16 @@
+WITH base AS (
 SELECT
   job,
   division,
   SUM(IFNULL(jobHours, 0)) jobHours,
   SUM(IFNULL(Profiles.defaultChargeOutRate,0) * IFNULL(jobHours, 0)) jobValue,
+  SUM(SUM(IFNULL(Profiles.defaultChargeOutRate,0) * IFNULL(jobHours, 0))) over() total,
   SUM(IFNULL(hours, 0)) hours
 FROM TimeEntries
   LEFT OUTER JOIN TimeSheets ON TimeEntries.tsid = TimeSheets.id
   LEFT OUTER JOIN Profiles ON TimeSheets.uid = Profiles.id
 WHERE job REGEXP CONCAT("^", ?) AND date >= ? AND date <= ?
 GROUP BY job,
-  division;
+  division
+)
+SELECT job, division, jobHours, jobValue, total, ROUND((jobValue * 100 / total),1) as jobValuePercent, hours from base
