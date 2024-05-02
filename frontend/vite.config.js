cfb86e7f2ba3aec88038b501e0b5f6bad252c6a4
Dean Stamler <dev@stamler.ca>
2024-05-02 16:12:41 -0400

    address "Vite CJS Node API deprecated"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
+import { fileURLToPath, URL } from "url";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
