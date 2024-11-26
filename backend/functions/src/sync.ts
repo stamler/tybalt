af7f988c4e9cee01d8f2736fb97e5de58d886115
Dean Stamler <dev@stamler.ca>
2024-11-26 11:15:59 -0500

    add fields to Profiles sync

--- backend/functions/src/sync.ts
+++ backend/functions/src/sync.ts
@@ -553,7 +553,10 @@ export async function exportProfiles(mysqlConnection: Connection) {
   // Get specified fields from all profiles and store them in an array of arrays
   // to be used in the INSERT query
   const allProfilesQuerySnap = await db.collection("Profiles").get();
-  const profilesFields = ["id", "surname", "givenName", "openingDateTimeOff", "openingOP", "openingOV", "untrackedTimeOff", "timestamp", "defaultChargeOutRate"];
+  const profilesFields = [
+    "id", "surname", "givenName", "openingDateTimeOff", "openingOP", "openingOV", "untrackedTimeOff", "timestamp", "defaultChargeOutRate",
+    "email", "userSourceAnchor64", "userSourceAnchor", "mobilePhone", "jobTitle", "azureId", "salary", "defaultDivision", "managerUid", "managerName",
+  ];
   const now = new Date();
   const insertValues = allProfilesQuerySnap.docs.map((profileSnap) => {
     const profile = profileSnap.data();
@@ -577,7 +580,7 @@ export async function exportProfiles(mysqlConnection: Connection) {
   // deleted from Firestore will remain in MySQL. For this reason we delete all
   // rows where the timestamp is older than expected. We can safely assume that
   // 5 minutes prior to now is too old.
-  const q = `INSERT INTO Profiles (${profilesFields.toString()}) VALUES ? ON DUPLICATE KEY UPDATE surname=VALUES(surname), givenName=VALUES(givenName), openingDateTimeOff=VALUES(openingDateTimeOff), openingOP=VALUES(openingOP), openingOV=VALUES(openingOV), untrackedTimeOff=VALUES(untrackedTimeOff), timestamp=UTC_TIMESTAMP(), defaultChargeOutRate=VALUES(defaultChargeOutRate)`;
+  const q = `INSERT INTO Profiles (${profilesFields.toString()}) VALUES ? ON DUPLICATE KEY UPDATE surname=VALUES(surname), givenName=VALUES(givenName), openingDateTimeOff=VALUES(openingDateTimeOff), openingOP=VALUES(openingOP), openingOV=VALUES(openingOV), untrackedTimeOff=VALUES(untrackedTimeOff), timestamp=UTC_TIMESTAMP(), defaultChargeOutRate=VALUES(defaultChargeOutRate), email=VALUES(email), userSourceAnchor64=VALUES(userSourceAnchor64), userSourceAnchor=VALUES(userSourceAnchor), mobilePhone=VALUES(mobilePhone), jobTitle=VALUES(jobTitle), azureId=VALUES(azureId), salary=VALUES(salary), defaultDivision=VALUES(defaultDivision), managerUid=VALUES(managerUid), managerName=VALUES(managerName)`;
   try {
     await mysqlConnection.query(q, [insertValues]);
   } catch (error) {
